6410cef4519960fac16763c8a25bfd8db94d98a8
Liam <liamreadsspam@gmail.com>
2022-08-04 21:09:12 -0300

    Prototype audio fun

--- Scripts/Player.gd
+++ Scripts/Player.gd
@@ -12,3 +12,24 @@ func _process(delta):
 	if Input.is_action_just_pressed("map"):
 		var map = get_parent().get_node("Map")
 		map.toggle()
+	
+	if Input.is_action_just_pressed("tone_a"):
+		$ToneA.play()
+	elif Input.is_action_just_released("tone_a"):
+		$ToneA.stop()
+	
+	if Input.is_action_just_pressed("tone_b"):
+		$ToneB.play()
+	elif Input.is_action_just_released("tone_b"):
+		$ToneB.stop()
+		
+	if Input.is_action_just_pressed("tone_c"):
+		$ToneC.play()
+	elif Input.is_action_just_released("tone_c"):
+		$ToneC.stop()
+	
+	if Input.is_action_just_pressed("tone_d"):
+		$ToneD.play()
+	elif Input.is_action_just_released("tone_d"):
+		$ToneD.stop()
+	
