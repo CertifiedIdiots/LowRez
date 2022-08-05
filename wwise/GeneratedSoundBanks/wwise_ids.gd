94534f5c881a424b7e668b40b33c465e16d71453
noahneras <110417604+noahneras@users.noreply.github.com>
2022-08-05 17:09:44 -0400

    Wwise integration

---
+++ wwise/GeneratedSoundBanks/wwise_ids.gd
@@ -0,0 +1,62 @@
+class_name AK
+
+class EVENTS:
+
+	const MUSIC = 3991942870 
+
+	const _dict = { 
+	 "MUSIC": MUSIC
+	} 
+
+class STATES:
+
+	const _dict = {} 
+
+class SWITCHES:
+
+	const _dict = {} 
+
+class GAME_PARAMETERS:
+
+	const _dict = {} 
+
+class TRIGGERS:
+
+	const _dict = {} 
+
+class BANKS:
+
+	const INIT = 1355168291 
+	const MUSIC = 3991942870 
+
+	const _dict = { 
+	 "INIT": INIT,
+	 "MUSIC": MUSIC
+	} 
+
+class BUSSES:
+
+	const MASTER_AUDIO_BUS = 3803692087 
+
+	const _dict = { 
+	 "MASTER AUDIO BUS": MASTER_AUDIO_BUS
+	} 
+
+class AUX_BUSSES:
+
+	const _dict = {} 
+
+class AUDIO_DEVICES:
+
+	const NO_OUTPUT = 2317455096 
+	const SYSTEM = 3859886410 
+
+	const _dict = { 
+	 "NO OUTPUT": NO_OUTPUT,
+	 "SYSTEM": SYSTEM
+	} 
+
+class EXTERNAL_SOURCES:
+
+	const _dict = {} 
+
