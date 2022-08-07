52d115a796f080ebbc489bbad337a4a1aaf0e09a
cicetil <cicetil@gmail.com>
2022-08-07 11:18:41 -0400

    implement interactable test

---
+++ Scripts/Actions.gd
@@ -0,0 +1,12 @@
+extends Area2D
+
+	
+# DOOR
+func open():
+	self.visible = false
+func close():
+	self.visible = true
+
+
+func _ready():
+	pass # Replace with function body.
--- Scripts/Player.gd
+++ Scripts/Player.gd
@@ -12,7 +12,7 @@ var notes = {
 func _physics_process(delta):
 	velocity = Input.get_vector("move_left", "move_right", "move_up", "move_down") * speed
 	velocity = move_and_slide(velocity)
-	print(self.position)
+#	print(self.position)
 
 func _process(delta):
 	if Input.is_action_just_pressed("map"):
@@ -27,3 +27,18 @@ func _process(delta):
 		if Input.is_action_just_pressed(note):
 			get_node(note).play()
 			notes[note] = 1
+		AB()
+
+# SEQUENCES
+func AB():
+	var door = get_node("/root/Main/TileMap/Door")
+	var player = get_node("/root/Main/Player")
+	var position = player.position
+	var distance = self.position.distance_to(door.position)
+	var complete = false
+	
+	if notes["tone_a"] > 0 and notes["tone_b"] > notes["tone_a"] and distance < 20.0:
+		door.open()
+		print("ayyy")
+	else:
+		door.close()
