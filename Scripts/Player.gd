f80592847bab617a29392b5a7b0ed068c3c7bfd9
Liam <liamreadsspam@gmail.com>
2022-08-07 01:20:05 -0300

    piano

--- Scripts/Player.gd
+++ Scripts/Player.gd
@@ -2,6 +2,12 @@ extends KinematicBody2D
 
 export var speed = 30
 var velocity = Vector2.ZERO
+var notes = {
+	"tone_a": 0,
+	"tone_b": 0,
+	"tone_c": 0,
+	"tone_d": 0
+}
 
 func _physics_process(delta):
 	velocity = Input.get_vector("move_left", "move_right", "move_up", "move_down") * speed
@@ -13,23 +19,11 @@ func _process(delta):
 		var map = get_parent().get_node("Map")
 		map.toggle()
 	
-	if Input.is_action_just_pressed("tone_a"):
-		$ToneA.play()
-	elif Input.is_action_just_released("tone_a"):
-		$ToneA.stop()
-	
-	if Input.is_action_just_pressed("tone_b"):
-		$ToneB.play()
-	elif Input.is_action_just_released("tone_b"):
-		$ToneB.stop()
-		
-	if Input.is_action_just_pressed("tone_c"):
-		$ToneC.play()
-	elif Input.is_action_just_released("tone_c"):
-		$ToneC.stop()
-	
-	if Input.is_action_just_pressed("tone_d"):
-		$ToneD.play()
-	elif Input.is_action_just_released("tone_d"):
-		$ToneD.stop()
-	
+	for note in notes.keys():
+		notes[note] = notes[note] - delta
+		if notes[note] <= 0:
+			notes[note] = 0
+			get_node(note).stop()
+		if Input.is_action_just_pressed(note):
+			get_node(note).play()
+			notes[note] = 1
