7396c26e41518cb3e135b83db26d285e59706afd
Liam <liamreadsspam@gmail.com>
2022-08-07 23:32:03 -0300

    player animation

--- Scripts/Player.gd
+++ Scripts/Player.gd
@@ -1,21 +1,22 @@
 extends KinematicBody2D
 
+var unlocked_map = false
 export var speed = 30
 var velocity = Vector2.ZERO
 var notes = {
-	"tone_a": 0,
-	"tone_b": 0,
-	"tone_c": 0,
-	"tone_d": 0
+
 }
 
 func _physics_process(delta):
 	velocity = Input.get_vector("move_left", "move_right", "move_up", "move_down") * speed
 	velocity = move_and_slide(velocity)
-#	print(self.position)
+	if velocity.x > 0:
+		$Sprite.flip_h = true
+	elif velocity.x < 0:
+		$Sprite.flip_h = false
 
 func _process(delta):
-	if Input.is_action_just_pressed("map"):
+	if unlocked_map and Input.is_action_just_pressed("map"):
 		var map = get_parent().get_node("Map")
 		map.toggle()
 	
